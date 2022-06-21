6e129106b735c68e6dacab9afc8f19e84c6a364d
yt8492 <tmymyut2000@gmail.com>
2022-06-21 21:47:52 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,72 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget
+import org.jetbrains.compose.experimental.dsl.IOSDevices
+
+plugins {
+    kotlin("multiplatform") version "1.6.21"
+    id("org.jetbrains.compose") version "1.2.0-alpha01-dev675"
+}
+
+group = "com.yt8492"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    gradlePluginPortal()
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    google()
+}
+
+kotlin {
+    iosX64("uikitX64") {
+        binaries {
+            executable {
+                entryPoint = "main"
+                freeCompilerArgs += listOf(
+                    "-linker-option", "-framework", "-linker-option", "Metal",
+                    "-linker-option", "-framework", "-linker-option", "CoreText",
+                    "-linker-option", "-framework", "-linker-option", "CoreGraphics"
+                )
+            }
+        }
+    }
+
+    sourceSets {
+        val uikitX64Main by getting {
+            dependencies {
+                implementation(kotlin("stdlib-common"))
+                implementation(compose.ui)
+                implementation(compose.foundation)
+                implementation(compose.material)
+                implementation(compose.runtime)
+            }
+        }
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
+}
+
+compose.experimental {
+    web.application {}
+    uikit.application {
+        bundleIdPrefix = "com.yt8492"
+        projectName = "Counter"
+        deployConfigurations {
+            simulator("IPhone8") {
+                //Usage: ./gradlew iosDeployIPhone8Debug
+                device = IOSDevices.IPHONE_8
+            }
+        }
+    }
+}
+
+kotlin {
+    targets.withType<KotlinNativeTarget> {
+        binaries.all {
+            freeCompilerArgs += "-Xdisable-phases=VerifyBitcode"
+        }
+    }
+}
---
+++ src/uikitX64Main/kotlin/main.kt
@@ -0,0 +1,101 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.window.Application
+import kotlinx.cinterop.ObjCObjectBase
+import kotlinx.cinterop.autoreleasepool
+import kotlinx.cinterop.cstr
+import kotlinx.cinterop.memScoped
+import kotlinx.cinterop.toCValues
+import platform.Foundation.NSStringFromClass
+import platform.UIKit.UIApplication
+import platform.UIKit.UIApplicationDelegateProtocol
+import platform.UIKit.UIApplicationDelegateProtocolMeta
+import platform.UIKit.UIApplicationMain
+import platform.UIKit.UIResponder
+import platform.UIKit.UIResponderMeta
+import platform.UIKit.UIScreen
+import platform.UIKit.UIWindow
+
+fun main() {
+    val args = emptyArray<String>()
+    memScoped {
+        val argc = args.size + 1
+        val argv = (arrayOf("skikoApp") + args).map { it.cstr.ptr }.toCValues()
+        autoreleasepool {
+            UIApplicationMain(argc, argv, null, NSStringFromClass(SkikoAppDelegate))
+        }
+    }
+}
+
+class SkikoAppDelegate : UIResponder, UIApplicationDelegateProtocol {
+    companion object : UIResponderMeta(), UIApplicationDelegateProtocolMeta
+
+    @ObjCObjectBase.OverrideInit
+    constructor() : super()
+
+    private var _window: UIWindow? = null
+    override fun window() = _window
+    override fun setWindow(window: UIWindow?) {
+        _window = window
+    }
+
+    override fun application(application: UIApplication, didFinishLaunchingWithOptions: Map<Any?, *>?): Boolean {
+        window = UIWindow(frame = UIScreen.mainScreen.bounds)
+        window!!.rootViewController = Application("Counter") {
+            App()
+        }
+        window!!.makeKeyAndVisible()
+        return true
+    }
+}
+
+@Composable
+fun App() {
+    val (count, setCount) = remember {
+        mutableStateOf(0)
+    }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text("CounterApp")
+                }
+            )
+        }
+    ) { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            Button(
+                onClick = {
+                    setCount(count + 1)
+                }
+            ) {
+                Text("+")
+            }
+            Text(count.toString())
+            Button(
+                onClick = {
+                    setCount(count - 1)
+                }
+            ) {
+                Text("-")
+            }
+        }
+    }
+}
